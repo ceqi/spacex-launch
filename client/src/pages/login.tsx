a4657055932d126a624c9cf8e2765ea87c8cccb5
ce.qi <ce.qi@outlook.com>
2020-08-21 00:32:22 +0800

    update login

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import {gql, ApolloClient, useMutation} from '@apollo/client';
+
+import { LoginForm } from '../components';
+import * as LoginTypes from './__generated__/login';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!) {
+    login(email: $email)
+  }
+`;
 
 export default function Login() {
-  return <div />;
+  const [login, { data }] = useMutation<LoginTypes.login, LoginTypes.loginVariables> (LOGIN_USER);
+  return <LoginForm login={login} />
 }
